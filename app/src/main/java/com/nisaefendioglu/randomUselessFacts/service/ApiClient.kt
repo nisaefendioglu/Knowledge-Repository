6cd6cf190221418dc968fa9e9d0a71f8cdb34f7d
nisaefendioglu <nisaefendioglu0@gmail.com>
2023-01-08 21:03:41 +0300

    ✍🏻 Edit ApiClient

--- app/src/main/java/com/nisaefendioglu/randomUselessFacts/service/ApiClient.kt
+++ app/src/main/java/com/nisaefendioglu/randomUselessFacts/service/ApiClient.kt
@@ -1,21 +1,15 @@
 package com.nisaefendioglu.randomUselessFacts.service
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiClient {
-
-    companion object {
-
-        var BASE_URL = "https://uselessfacts.jsph.pl/"
-
-        fun create(java: Class<ApiInterface>): ApiInterface {
-
-            val retrofit = Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(BASE_URL)
-                .build()
-            return retrofit.create(ApiInterface::class.java)
-
-        }
+object ApiClient {
+    private const val BASE_URL = "https://uselessfacts.jsph.pl/"
+    fun create(apiInterface: Class<ApiInterface>): ApiInterface {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(apiInterface)
     }
 }
