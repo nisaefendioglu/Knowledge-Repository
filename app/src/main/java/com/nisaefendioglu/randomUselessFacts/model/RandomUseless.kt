1200b0d0daaec54a542e2c146dc9edfc85b720fd
nisaefendioglu <nisaefendioglu0@gmail.com>
2023-01-10 21:04:51 +0300

    ✨ Edit model

--- app/src/main/java/com/nisaefendioglu/randomUselessFacts/model/RandomUseless.kt
+++ app/src/main/java/com/nisaefendioglu/randomUselessFacts/model/RandomUseless.kt
@@ -1,19 +1,10 @@
 package com.nisaefendioglu.randomUselessFacts.model
 
-
 import com.google.gson.annotations.SerializedName
 
 data class RandomUseless(
-    @SerializedName("id")
-    val id: String,
-    @SerializedName("language")
-    val language: String,
-    @SerializedName("permalink")
-    val permalink: String,
     @SerializedName("source")
     val source: String,
-    @SerializedName("source_url")
-    val sourceUrl: String,
     @SerializedName("text")
     val text: String
 )
